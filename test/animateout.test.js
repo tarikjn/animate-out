da06060a5f7d2d7fe1fc7df9ee49ee11f66f36bc
Zack Stayman <zack@stayman.co>
2016-06-21 16:48:09 -0400

    basics of testing in place

---
+++ test/animateout.test.js
@@ -0,0 +1,13 @@
+var React = require('react');
+var expect = require('chai').expect;
+
+describe('AnimateOut', function() {
+  it('should work', function() {
+    expect(true).to.be.true;
+  });
+
+  it('should work with jsx', function() {
+    var x = <div />;
+    expect(x).to.be.a.function;
+  });
+});
