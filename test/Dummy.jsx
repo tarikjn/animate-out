8dc7da01472dbc4f6c3859fdcc978a87ba1a4eef
Patrick Steadman <ptsteadman@gmail.com>
2016-07-20 16:08:52 -0400

    fix remounting of component when completing issue

--- test/Dummy.jsx
+++ test/Dummy.jsx
@@ -4,16 +4,10 @@ module.exports = React.createClass({
   propTypes: {
     handleMount: React.PropTypes.func,
   },
-  getInitialState() {
-    console.log('state');
-    return {};
-  },
   componentDidMount() {
     this.props.handleMount();
-    console.log('mounted');
   },
   render: function() {
-    console.log('render');
     return <div />;
   }
 })
